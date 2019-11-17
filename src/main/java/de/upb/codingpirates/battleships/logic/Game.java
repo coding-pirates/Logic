aaa9a1b829f6435bed2def043a67b855dd7c868f
Roman Lamichhane <rlamich2>
2019-11-17 18:22:38 +0100

    filled Class Game

--- src/main/java/de/upb/codingpirates/battleships/logic/Game.java
+++ src/main/java/de/upb/codingpirates/battleships/logic/Game.java
@@ -1,5 +1,63 @@
 package de.upb.codingpirates.battleships.logic;
 
+import javafx.util.Pair;
+import java.util.List;
+
 public class Game {
+
+    private int id;
+    private String name;
+    private int playerCount;
+    private GameState gameState;
+    private GameConfiguration config;
+    private List<Player> players;
+    private List<Field> fields;
+    private long timeLeft;
+    private int round;
+
+
+    public Game(GameConfiguration config) // unsicher ob Datentyp Gameconfiguration or Configuration
+    {
+
+        this.config = config;
+    }
+
+    public boolean areShotsLeft(Player player)
+    {
+        //TODO
+        return false;
+
+    }
+    public boolean areAllShipsSunken(Player player)
+    {
+        //TODO
+        return false;
+    }
+    public boolean isTimeLeft()
+    {
+        //TODO
+        return false;
+    }
+
+    public boolean areShipsIndepentent(List<Ship> Ships)
+    {
+        //TODO
+        return false;
+    }
+    public void addPlayer(Player player)
+    {
+        //TODO
+    }
+
+    private void addField(Player player)
+    {
+        //TODO
+    }
+    public List<Pair<Player,String>> getFinalScore()
+    {
+        //TODO
+        return null;
+
+    }
 
 }
