d16e480cde446b1f3de7465eb717f30eb7e3d9e2
carolinm <carolinm@campus.upb.de>
2019-11-19 16:22:02 +0100

    added enum classes

---
+++ src/main/java/de/upb/codingpirates/battleships/logic/ClientType.java
@@ -0,0 +1,5 @@
+package de.upb.codingpirates.battleships.logic;
+
+public enum ClientType {
+    PLAYER, SPECTATOR, ENGINEPLAYER
+}
---
+++ src/main/java/de/upb/codingpirates/battleships/logic/GameState.java
@@ -0,0 +1,5 @@
+package de.upb.codingpirates.battleships.logic;
+
+public enum GameState {
+        LOBBY, IN_PROGRESS, PAUSED, FINISHED
+}
