514c3ade5f496882a4f5e2e06a56c5a645ba99fd
Roman Lamichhane <rlamich2@mail.uni-paderborn.de>
2019-11-19 18:39:34 +0100

    Update Game.java

--- src/main/java/de/upb/codingpirates/battleships/logic/Game.java
+++ src/main/java/de/upb/codingpirates/battleships/logic/Game.java
@@ -60,8 +60,12 @@ public class Game {
 
     }
     
-      public int getId() { //getter ID
+    public int getId() { //getter ID
         return id;
+    }
+    
+    public void setId(int newID){
+        this.id = newID;
     }
 
     public String getName() {//getter Name
@@ -91,25 +95,50 @@ public class Game {
     public List<Player> getPlayers() { //getter Players from game
         return players;
     }
+    
+    public void addPlayer(Player newPlayer){ //add Players to list
+        players.add(newPlayer);
+    }
 
     public int getRound() { //getter Roundnumber
         return round;
+    }
+    
+    public void setRound(int newRound){ //setter Round
+        this.round = newRound;
     }
 
     public GameState getState() { //getter Gamestate
         return state;
     }
-
+    
+    public void setState(Gamestate newState){ //setter State
+        this.state = newState;
+    }
+    
     public Configuration getConfig() { //getter Configuration
         return config;
     }
-
+    
+    public void setConfiguration(Configuration config){ //setter Configuration
+        this.config = config;
+    }
+    
     public List<Field> getFields() {//getter fields
         return fields;
     }
+    
+    public void setField(Field newField){//add new Field
+        fields.add(newField);
+    }
+    
 
     public long getTimeLeft() { //getter left time
         return timeLeft;
+    }
+    
+    public void setTimeLeft(long time){ //setter TimeLeft
+        this.timeLeft = time;
     }
 
 }
