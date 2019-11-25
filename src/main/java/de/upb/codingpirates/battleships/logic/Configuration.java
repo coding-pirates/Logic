58c858ef3e3adb9261bdaccaa2583b7d59d9e6c3
Paul Becker <paube@mail.upb.de>
2019-11-25 16:45:42 +0100

    make public fields private & add getter

--- src/main/java/de/upb/codingpirates/battleships/logic/Configuration.java
+++ src/main/java/de/upb/codingpirates/battleships/logic/Configuration.java
@@ -2,7 +2,6 @@ package de.upb.codingpirates.battleships.logic;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
-import com.google.gson.annotations.SerializedName;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
@@ -18,82 +17,116 @@ public class Configuration {
     /**
      * max player possible in one game
      */
-    @SerializedName("maxPlayerCount")
-    public final int MAXPLAYERCOUNT;
+    private final int maxPlayerCount;
     /**
      * Specifies the height of the playing field
      */
-    @SerializedName("height")
-    public final int HEIGHT;
+    private final int height;
     /**
      * Specifies the width of the playing field
      */
-    @SerializedName("width")
-    public final int WIDTH;
+    private final int width;
     /**
      * Gives the number of possible shots per
      * Round on
      */
-    @SerializedName("shotCount")
-    public final int SHOTCOUNT;
+    private final int shotCount;
     /**
      * Indicates the number of points a hit gives
      */
-    @SerializedName("hitPoints")
-    public final int HITPOINTS;
+    private final int hitPoints;
     /**
      * Indicates the number of points a sunken ship gives
      */
-    @SerializedName("sunkPoints")
-    public final int SUNKPOINTS;
+    private final int sunkPoints;
     /**
      * time to place the shots
      */
-    @SerializedName("roundTime")
-    public final long ROUNDTIME;
+    private final long roundTime;
     /**
      * Sets the time for animations to run.
      * before the timer for
      * the roundTime starts
      */
-    @SerializedName("visualizationTime")
-    public final long VISUALIZATIONTIME;
+    private final long visualizationTime;
     /**
      * Sets the points to subtract
      * if an invalid move is made
      */
-    @SerializedName("penaltyMinusPoints")
-    public final int PENALTYMINUSPOINTS;
+    private final int penaltyMinusPoints;
     /**
      * Sets the type of penalty for an invalid
      * Play
      */
-    @SerializedName("penaltyKind")
-    public final PenaltyType PENALTYTYPE;
+    private final PenaltyType penaltyKind;
     /**
      * A map that maps from unique ID
      * to a shipType
      */
     @Nonnull
-    @SerializedName("ships")
-    private final Map<Integer, ShipType> SHIPTYPES;
+    private final Map<Integer, ShipType> ships;
 
     public Configuration(int maxPlayerCount, int height, int width, int shotCount, int hitPoints, int sunkPoints, long roundTime, long visualizationTime, @Nonnull Map<Integer, ShipType> shipTypes, int penaltyMinusPoints, PenaltyType penaltyType) {
-        this.MAXPLAYERCOUNT = maxPlayerCount;
-        this.HEIGHT = height;
-        this.WIDTH = width;
-        this.SHOTCOUNT = shotCount;
-        this.HITPOINTS = hitPoints;
-        this.SUNKPOINTS = sunkPoints;
-        this.ROUNDTIME = roundTime;
-        this.VISUALIZATIONTIME = visualizationTime;
-        this.SHIPTYPES = ImmutableMap.copyOf(shipTypes);
-        this.PENALTYMINUSPOINTS = penaltyMinusPoints;
-        this.PENALTYTYPE = penaltyType;
+        this.maxPlayerCount = maxPlayerCount;
+        this.height = height;
+        this.width = width;
+        this.shotCount = shotCount;
+        this.hitPoints = hitPoints;
+        this.sunkPoints = sunkPoints;
+        this.roundTime = roundTime;
+        this.visualizationTime = visualizationTime;
+        this.ships = ImmutableMap.copyOf(shipTypes);
+        this.penaltyMinusPoints = penaltyMinusPoints;
+        this.penaltyKind = penaltyType;
+    }
+
+    @Nonnull
+    public Map<Integer, ShipType> getShipTypes() {
+        return ImmutableMap.copyOf(ships);
+    }
+
+    public int getMaxPlayerCount() {
+        return maxPlayerCount;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getShotCount() {
+        return shotCount;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getSunkPoints() {
+        return sunkPoints;
+    }
+
+    public long getRoundTime() {
+        return roundTime;
+    }
+
+    public long getVisualizationTime() {
+        return visualizationTime;
+    }
+
+    public int getPenaltyMinusPoints() {
+        return penaltyMinusPoints;
+    }
+
+    public PenaltyType getPenaltyKind() {
+        return penaltyKind;
     }
 
     @Nonnull
     public Map<Integer, ShipType> getShips() {
-        return ImmutableMap.copyOf(SHIPTYPES);
+        return ships;
     }
 }
