d4ed4d96d91eb9c301e13e322993e80502e2e29f
Paul Becker <paube@mail.upb.de>
2019-11-26 16:10:13 +0100

    add some logic changes

---
+++ src/main/java/de/upb/codingpirates/battleships/logic/BoundingBox.java
@@ -0,0 +1,23 @@
+package de.upb.codingpirates.battleships.logic;
+
+public class BoundingBox {
+    private final int x;
+    private final int y;
+
+    public BoundingBox(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getXSize() {
+        return x;
+    }
+
+    public int getYSize() {
+        return y;
+    }
+
+    public int getSize(){
+        return x*y;
+    }
+}
