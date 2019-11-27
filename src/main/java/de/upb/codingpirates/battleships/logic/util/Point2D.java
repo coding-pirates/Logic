8d2b9618169142ce62401d148611f58e8867e836
Andre Blanke <blanke.and@gmail.com>
2019-11-27 18:37:47 +0100

    Add equals and hashCode for Point2D

--- src/main/java/de/upb/codingpirates/battleships/logic/util/Point2D.java
+++ src/main/java/de/upb/codingpirates/battleships/logic/util/Point2D.java
@@ -3,7 +3,9 @@ package de.upb.codingpirates.battleships.logic.util;
 /**
  * Represents a point in a two-dimensional coordinate system
  *
- * @author Interdoc committee & Paul Becker
+ * @author Interdoc committee
+ * @author Paul Becker
+ * @author Andre Blanke
  */
 public class Point2D {
 
@@ -19,6 +21,20 @@ public class Point2D {
     public Point2D(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (!(object instanceof Point2D))
+            return false;
+        final Point2D other = (Point2D) object;
+
+        return (other.getX() == getX()) && (other.getY() == getY());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
     }
 
     public int getX() {
