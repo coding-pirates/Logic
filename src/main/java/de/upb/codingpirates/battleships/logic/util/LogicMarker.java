219d5a16ce85f1ad9b53bf8884838376fadb25a5
Paul Becker <paube@mail.upb.de>
2019-12-03 14:09:13 +0100

    modify field hit returntype

--- src/main/java/de/upb/codingpirates/battleships/logic/util/LogicMarker.java
+++
@@ -1,9 +0,0 @@
-package de.upb.codingpirates.battleships.logic.util;
-
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
-
-public class LogicMarker {
-    public static final Marker SHIPPLACEMENT = MarkerManager.getMarker("Ship Placement");
-    public static final Marker SHOTSPLACEMENT = MarkerManager.getMarker("Shot Placement");
-}
