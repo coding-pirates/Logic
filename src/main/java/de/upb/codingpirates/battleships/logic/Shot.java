0bb1062757e3621c568b683539b0f0a3a0cf616c
Benjamin Kasten <bkasten@mail.uni-paderborn.de>
2019-12-29 20:54:28 +0100

    added test for placeShots_1, added toString method in Shot.class, removed a test, some improvements

--- src/main/java/de/upb/codingpirates/battleships/logic/Shot.java
+++ src/main/java/de/upb/codingpirates/battleships/logic/Shot.java
@@ -63,4 +63,9 @@ public class Shot {
         }
         return false;
     }
+
+    @Override
+    public String toString(){
+        return "[ClientId: " + this.getClientId() + ", Point: " + this.getTargetField().toString() + "]";
+    }
 }
